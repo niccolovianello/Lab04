c1d58a6eb849de8db426c8d30fbae96ff3fef0ba
Niccolò Vianello <vianello.niccolo@gmail.com>
2019-03-27 12:11:26 +0100

    Start

--- Lab04_SegreteriaStudenti/src/it/polito/tdp/lab04/model/Model.java
+++ Lab04_SegreteriaStudenti/src/it/polito/tdp/lab04/model/Model.java
@@ -1,6 +1,15 @@
 package it.polito.tdp.lab04.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Model {
 
+	private List<Corso> corsi;
+	private List<Studente> studenti;
 	
+	public Model() {
+		this.corsi = new ArrayList<Corso>(); 
+		this.studenti = new ArrayList<Studente>(); 
+	}
 }
